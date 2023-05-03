52666990984388c4a3b56bfcd4fcd743c7b5298f
ikramul <ikramulsohel101@gmail.com>
2023-05-04 00:36:34 +0600

    updated

---
+++ src/pages/Calculator.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './calculator.scss'
+
+const Calculator = () => {
+  return (
+    <div className='calculator__container'>
+        <p>ca</p>
+    </div>
+  )
+}
+
+export default Calculator
